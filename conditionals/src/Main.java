35e6b94074dbe54ca99d0cc4504d7ec7183cb300
Cengiz Şen <58827136+CengizSen@users.noreply.github.com>
2022-06-13 14:14:21 +0300

    conditionals
    conditionals

---
+++ conditionals/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+ public static void main (String[]args){
+
+     int sayi=20;
+     if(sayi<20){
+         System.out.println("SAYI 20 DEN KUCUKTUR");
+     }
+      else if(sayi>20)  {
+         System.out.println("SAYI 20 DEN BUYUKTUR");
+
+     } else if (sayi==20) {
+         System.out.println("SAYI 20 Dir");
+
+     }
+      else {
+         System.out.println("Tanımsız Değer");
+
+     }
+
+
+ }
+
+}
